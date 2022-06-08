cbe5c0c488417686e296325c16a75c6516dbe565
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2022-06-08 16:18:49 -0500

    added sample project

---
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            
+            return View(dogs);
+        }
+
+        public string Hello()
+        {
+            return "Who is there!";
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Create()
+        {
+            var dog = new DogViewModel();
+            return View(dog);
+        }
+    }
+}
