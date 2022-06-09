0f66879deedc64ae7ea3fb025abd1bcfe4879d3b
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2022-06-09 15:39:02 -0500

    added pizza ording system

--- RazorPizzeria/RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzeria/RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet =true)]
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public decimal PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+                PizzaName = "Custom";
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+               ImageTitle = "Create";
+
         }
     }
 }
