54b50c679744036f8db20f34d91424ac83cb914f
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2022-06-10 15:22:43 -0500

    added Entity frame work and sql server

--- RazorPizzeria/RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzeria/RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Data;
 using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages.Checkout
@@ -7,17 +8,21 @@ namespace RazorPizzeria.Pages.Checkout
     [BindProperties(SupportsGet =true)]
     public class CheckoutModel : PageModel
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public CheckoutModel(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
         public string PizzaName { get; set; }
         public decimal PizzaPrice { get; set; }
         public string ImageTitle { get; set; }
 
         public void OnGet()
         {
-            if (string.IsNullOrWhiteSpace(PizzaName))
-                PizzaName = "Custom";
-            if (string.IsNullOrWhiteSpace(ImageTitle))
-               ImageTitle = "Create";
-
+            if (string.IsNullOrWhiteSpace(PizzaName)) PizzaName = "Custom";
+            if (string.IsNullOrWhiteSpace(ImageTitle)) ImageTitle = "Create";
+            _applicationDbContext.PizzaOrders.Add(new PizzaOrder { PizzaName = PizzaName, BasePrice = PizzaPrice });
+            _applicationDbContext.SaveChanges();
         }
     }
 }
