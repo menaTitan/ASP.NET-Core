9abb9da6d67c31ee6a0a8103043797425605256d
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2022-06-15 15:47:49 -0500

    added upload file projec

---
+++ File_Upload/File_Upload/Services/FileUpload.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace File_Upload.Services
+{
+    public interface IFileUpload
+    {
+        Task Uploadfile(IBrowserFile file);
+    }
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+        public  async Task Uploadfile(IBrowserFile file)
+        {
+            if (file is not null) 
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+                    //maxAllowedSize:122222222 -> allowed to upload bigger size file
+                    using (var stram = file.OpenReadStream(maxAllowedSize:122222222))
+                    {
+                        var fileStream = File.Create(uploadPath);
+
+                        await stram.CopyToAsync(fileStream);
+
+                        fileStream.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    _logger.LogError(e.ToString()); ;
+                }
+            
+            }
+        }
+    }
+}
