d5054bac2b230568725db3fa1e6341d4b922c65d
LOCALGOVCORP\Menas <menas@localgovcorp.com>
2022-06-17 16:43:14 -0500

    added file download functionality

---
+++ File_Upload/File_Upload/Services/FileDownload.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.JSInterop;
+
+namespace File_Upload.Services
+{
+    public interface IFileDownload
+    {
+        Task<List<string>> GetUploadFiles();
+        Task Download(string url);
+    }
+    public class FileDownload : IFileDownload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly IJSRuntime _js;
+
+        public FileDownload(IWebHostEnvironment webHostEnvironment, IJSRuntime js)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _js = js;
+        }
+
+        public async Task Download(string url)
+        {
+            await _js.InvokeVoidAsync("downloadFile", url);
+        }
+
+        public async Task<List<string>> GetUploadFiles()
+        {
+            var base64Urls = new List<string>();
+            var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+            var files = Directory.GetFiles(uploadPath);
+
+            if(files is not null && files.Length > 0)
+            {
+                foreach (var file in files)
+                {
+                    using (var fileInput = new FileStream(file, FileMode.Open, FileAccess.Read))
+                    {
+                        var memorystream = new MemoryStream();
+                        await fileInput.CopyToAsync(memorystream);
+                        
+                        var buffer = memorystream.ToArray();
+                        var fileType = GetMimeTypeForFileExtension(file);
+
+                        base64Urls.Add($"data:{fileType}; base64,{ Convert.ToBase64String(buffer)}");
+                    }   
+                }
+            }
+
+            return base64Urls;
+        }
+
+        private string GetMimeTypeForFileExtension(string filePath)
+        {
+            const string DetaulfContentType = "application/octet-stream";
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(filePath, out string contentType)) 
+            { 
+                contentType = DetaulfContentType;
+            }
+
+            return contentType;
+        }
+        private string GetFileName(string filePath)
+        {
+
+            return Path.GetFileName(filePath);
+
+        }
+
+     
+    }
+
+}
